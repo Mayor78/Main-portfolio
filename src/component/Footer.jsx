0c39dda0d7da6a3c805fb7bcf87675b36303c310
Abraham mayowa <Abrahammayowa78@gmail.com>
2024-04-11 12:57:26 +0100

    icon added

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -7,12 +7,12 @@ const Footer = () => {
     <div>
         <div className="max-w-[1200px] sm:h-[150px] p-12 flex justify-between mx-auto">
             <span className='primary-color'>A. Mayor</span>
-            <p className='text-gray-600 hover:text-white cursor-pointer'>Abrahammayowa78@gmail.com <br />#webDev2024</p>
+            <p className='text-gray-600 hover:text-white cursor-pointer'>Abrahammayowa78@gmail.com <br /></p>
         </div>
-        <div className=" text-white h-[20px]  cursor-pointer md:mr-4   items-center justify-center  md:h-[100px] max-w-[1200px] mx-auto grid grid-cols-6 ">
-          <FaGithub/>
-          <MdWhatsapp />
-          <FaXTwitter />
+        <div className=" text-white h-[20px]  flex py-3 items-center justify-center mb-5 ">
+         <a href="https://github.com/Mayor78"> <FaGithub className='5-xl m-10 hover:bg-primary-color '/></a>
+         <a href=""> <MdWhatsapp className='5-xl m-8 ' /></a>
+         <a href=""><FaXTwitter className='5-xl m-8 '/></a>
 
         </div>
     
